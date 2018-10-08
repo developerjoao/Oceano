9d1c2b75a8f85e56b29366378910166a18ab1ac8
Joao <jgazbral@gmail.com>
2018-10-08 20:30:29 +0000

    Metodos de verificar adjacentes e de simulação implementados mais teste com sardinha

--- Simulacao-Parte1/src/Ocean.java
+++ Simulacao-Parte1/src/Ocean.java
@@ -1,9 +1,17 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * @author João Cabral e Sandino
  */
 public class Ocean
 {
+	
+	 // A random number generator for providing random locations.
+     private static final Random rand = Randomizer.getRandom();
+     
 	 private int height, width;
 	 
 	 private Object[][] ocean;
@@ -69,7 +77,7 @@ public class Ocean
     
     /**
      * Place an fish at the given location.
-     * If there is already an animal at the location it will
+     * If there is already an fish at the location it will
      * be lost.
      * @param fish The fish to be placed.
      * @param row Row coordinate of the location.
@@ -84,11 +92,110 @@ public class Ocean
      * Place an fish at the given location.
      * If there is already an fish at the location it will
      * be lost.
-     * @param fish The animal to be placed.
-     * @param location Where to place the animal.
+     * @param fish The fish to be placed.
+     * @param location Where to place the fish.
      */
     public void place(Object fish, Location location)
     {
         ocean[location.getRow()][location.getCol()] = fish;
+    }
+    
+    public Location randomAdjacentLocation(Location location)
+    {
+        List<Location> adjacent = adjacentLocations(location);
+        return adjacent.get(0);
+    }
+    
+    /**
+     * Return the fish at the given location, if any.
+     * @param location Where in the field.
+     * @return The fish at the given location, or null if there is none.
+     */
+    public Object getObjectAt(Location location)
+    {
+        return getObjectAt(location.getRow(), location.getCol());
+    }
+    
+    /**
+     * Return the fish at the given location, if any.
+     * @param row The desired row.
+     * @param col The desired column.
+     * @return The fish at the given location, or null if there is none.
+     */
+    public Object getObjectAt(int row, int col)
+    {
+        return ocean[row][col];
+    }
+    
+    /**
+     * Get a shuffled list of the free adjacent locations.
+     * @param location Get locations adjacent to this.
+     * @return A list of free adjacent locations.
+     */
+    public List<Location> getFreeAdjacentLocations(Location location)
+    {
+        List<Location> free = new LinkedList<Location>();
+        List<Location> adjacent = adjacentLocations(location);
+        for(Location next : adjacent) {
+            if(getObjectAt(next) == null) {
+                free.add(next);
+            }
+        }
+        return free;
+    }
+    
+    /**
+     * Try to find a free location that is adjacent to the
+     * given location. If there is none, return null.
+     * The returned location will be within the valid bounds
+     * of the field.
+     * @param location The location from which to generate an adjacency.
+     * @return A valid location within the grid area.
+     */
+    public Location freeAdjacentLocation(Location location)
+    {
+        // The available free ones.
+        List<Location> free = getFreeAdjacentLocations(location);
+        if(free.size() > 0) {
+            return free.get(0);
+        }
+        else {
+            return null;
+        }
+    }
+
+    /**
+     * Return a shuffled list of locations adjacent to the given one.
+     * The list will not include the location itself.
+     * All locations will lie within the grid.
+     * @param location The location from which to generate adjacencies.
+     * @return A list of locations adjacent to that given.
+     */
+    public List<Location> adjacentLocations(Location location)
+    {
+        assert location != null : "Null location passed to adjacentLocations";
+        // The list of locations to be returned.
+        List<Location> locations = new LinkedList<Location>();
+        if(location != null) {
+            int row = location.getRow();
+            int col = location.getCol();
+            for(int roffset = -1; roffset <= 1; roffset++) {
+                int nextRow = row + roffset;
+                if(nextRow >= 0 && nextRow < height) {
+                    for(int coffset = -1; coffset <= 1; coffset++) {
+                        int nextCol = col + coffset;
+                        // Exclude invalid locations and the original location.
+                        if(nextCol >= 0 && nextCol < width && (roffset != 0 || coffset != 0)) {
+                            locations.add(new Location(nextRow, nextCol));
+                        }
+                    }
+                }
+            }
+            
+            // Shuffle the list. Several other methods rely on the list
+            // being in a random order.
+            Collections.shuffle(locations, rand);
+        }
+        return locations;
     }
 }
--- Simulacao-Parte1/src/Simulator.java
+++ Simulacao-Parte1/src/Simulator.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 /**
  * @author João Cabral e Sandino
@@ -8,10 +10,11 @@ import java.util.List;
 
 public class Simulator
 {
+	private static final double FISH_CREATION_PROBABILITY = 0.02;
 	// Constants representing configuration information for the simulation.
     // The default width for the grid.
     private static final int DEFAULT_WIDTH = 50;
-    // The default depth of the grid.
+    // The default height of the grid.
     private static final int DEFAULT_HEIGHT = 50;
 	
 	private List<Fish> fishes;
@@ -51,19 +54,89 @@ public class Simulator
     
     public void run(int steps)
     {
-        // put the simulation main loop here
+        runLongSimulation();
         
-        simView.showStatus(0, ocean);
+        //simView.showStatus(0, ocean);
     }
 
+    /**
+     * Run the simulation from its current state for a reasonably long period,
+     * e.g. 500 steps.
+     */
+    public void runLongSimulation()
+    {
+        simulate(500);
+    }
+    
+    /**
+     * Run the simulation from its current state for the given number of steps.
+     * Stop before the given number of steps if it ceases to be viable.
+     * @param numSteps The number of steps to run for.
+     */
+    public void simulate(int numSteps)
+    {
+        for(int step = 1; step <= numSteps && simView.isViable(ocean); step++) {
+            simulateOneStep();
+        }
+    }
+    
+    /**
+     * Run the simulation from its current state for a single step.
+     * Iterate over the whole field updating the state of each
+     * fox and rabbit.
+     */
+    public void simulateOneStep()
+    {
+        step++;
+
+        // Provide space for newborn animals.
+        List<Fish> newFishes = new ArrayList<Fish>();        
+        // Let all fishes act.
+        for(Iterator<Fish> it = fishes.iterator(); it.hasNext(); ) {
+            Fish fish = it.next();
+            fish.act(newFishes);
+            if(! fish.isAlive()) {
+                it.remove();
+            }
+        }
+               
+        // Add the newly born foxes and rabbits to the main lists.
+        fishes.addAll(newFishes);
+
+        simView.showStatus(step, ocean);
+    }
 
 	public void reset()
 	{
 		step = 0;
 		fishes.clear();
-		//populate();
+		populate();
     
 		// Show the starting state in the view.
 		simView.showStatus(step, ocean);
 	}
+	
+	/**
+     * Randomly populate the ocean with foxes and rabbits.
+     */
+    private void populate()
+    {
+        Random rand = Randomizer.getRandom();
+        ocean.clear();
+        for(int row = 0; row < ocean.getHeight(); row++) {
+            for(int col = 0; col < ocean.getWidth(); col++) {
+                if(rand.nextDouble() <= FISH_CREATION_PROBABILITY) {
+                    Location location = new Location(row, col);
+                    Fish fish = new Sardine(true, ocean, location);
+                    fishes.add(fish);
+                }
+//                else if(rand.nextDouble() <= RABBIT_CREATION_PROBABILITY) {
+//                    Location location = new Location(row, col);
+//                    Rabbit rabbit = new Rabbit(true, ocean, location);
+//                    animals.add(rabbit);
+//                }
+                // else leave the location empty.
+            }
+        }
+    }
 }
