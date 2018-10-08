d41cb9ddf39bcdd233879a83f8e0947dc3372dfa
Joao <jgazbral@gmail.com>
2018-10-08 13:02:11 +0000

    Implementação do metodo reset e do construtor do simulator

--- Simulacao-Parte1/src/Simulator.java
+++ Simulacao-Parte1/src/Simulator.java
@@ -1,11 +1,23 @@
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * (Fill in description and author info here)
+ * @author João Cabral e Sandino
  */
 
 public class Simulator
 {
+	// Constants representing configuration information for the simulation.
+    // The default width for the grid.
+    private static final int DEFAULT_WIDTH = 50;
+    // The default depth of the grid.
+    private static final int DEFAULT_HEIGHT = 50;
+	
+	private List<Fish> fishes;
+	
+	private int step;
+	
     private Ocean ocean;
     private SimulatorView simView;
     
@@ -20,11 +32,21 @@ public class Simulator
     
     public Simulator(int height, int width)
     {
+    	if(height <= 0 || width <= 0) {
+            System.out.println("The dimensions must be greater than zero.");
+            System.out.println("Using default values.");
+            height = DEFAULT_HEIGHT;
+            width = DEFAULT_WIDTH;
+        }
+    	fishes = new ArrayList<Fish>();
+    	
         ocean = new Ocean(height, width);
         simView = new SimulatorView(height, width);
         
         // define in which color fish should be shown
         simView.setColor(Fish.class, Color.red);
+        
+        reset();
     }
     
     public void run(int steps)
@@ -33,4 +55,15 @@ public class Simulator
         
         simView.showStatus(0, ocean);
     }
+
+
+	public void reset()
+	{
+		step = 0;
+		fishes.clear();
+		//populate();
+    
+		// Show the starting state in the view.
+		simView.showStatus(step, ocean);
+	}
 }
